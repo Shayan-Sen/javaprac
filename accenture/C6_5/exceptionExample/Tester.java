6d0b026c6cf5eb1290461e30f4439fafdb49a28d
Shayan Sen <senshayan2103@gmail.com>
2025-10-16 02:24:58 +0530

    add: implement Phone class with validation and Tester class for demonstration

---
+++ accenture/C6_5/exceptionExample/Tester.java
@@ -0,0 +1,15 @@
+package accenture.C6_5.exceptionExample;
+
+public class Tester {
+    public static void main(String[] args) {
+        String[] numbers = {"1234567890", null, "0987654321", "5678901234","2345678901"};
+        for (String number : numbers) {
+            try {
+                Phone phone = new Phone("iPhone", number);
+                System.out.println(phone);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error creating phone: " + e.getLocalizedMessage());
+            }
+        }
+    }
+}
