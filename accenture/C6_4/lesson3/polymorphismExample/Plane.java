784d48beee54e1ae741437d17480df2f6f86bcc2
Shayan Sen <senshayan2103@gmail.com>
2025-10-15 23:25:37 +0530

    add: implement Vehicle, Car, Boat, and Plane classes with basic functionality

---
+++ accenture/C6_4/lesson3/polymorphismExample/Plane.java
@@ -0,0 +1,8 @@
+package accenture.C6_4.lesson3.polymorphismExample;
+
+public class Plane extends Vehicle {
+    Plane(){
+        super("Plane start", "Plane stop", "Plane speed", "Plane direction");
+    }
+    
+}
