760e2acc835e497a34ca14e36e92e62beddc568a
Shayan Sen <senshayan2103@gmail.com>
2025-10-15 12:45:00 +0530

    add: create JavaDocExample class with Hello method and main method

---
+++ accenture/C6_2/JavaDocExample.java
@@ -0,0 +1,28 @@
+package accenture.C6_2;
+
+/**
+ * This is my first JavaDoc
+ * @author Shayan Sen
+ */
+public class JavaDocExample {
+    
+    /**
+     * This is my first method
+     * @param name Name of the person
+     * @param age Age of the person
+     * @return Nothing
+     */
+    public static void Hello(String name,int age){
+        // Prints Hello message
+        System.out.printf("Hello everyone, I am %s. I am %d years old\n",name,age);
+    }
+
+    /**
+     * Main method
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        // Should print  - Hello everyone, I am Shayan Sen. I am 22 years old
+        Hello("Shayan Sen", 22);
+    }
+}
