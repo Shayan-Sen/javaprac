efe9d509d075e0388ff1ad356b5951c5b14a4ac3
Shayan Sen <senshayan2103@gmail.com>
2025-10-15 09:40:53 +0530

    add: implement dynamic countdown in Fifteen.java

--- accenture/C6_2/Fifteen.java
+++ accenture/C6_2/Fifteen.java
@@ -1,11 +1,18 @@
 package accenture.C6_2;
 
+import java.util.Scanner;
+
 public class Fifteen {
 
     public static void main(String[] args) {
-        int[] numbers = {1,2,3,4,5};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter liftoff countdown : ");
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        for(int i = 1; i <= n; i++ ){ numbers[i-1] = n-i+1; }
         String[] words = {"Ignition sequence start!","Liftoff"};
-
+        scanner.close();
+        System.out.println();
         System.out.println(words[0]);
         for (int number : numbers) {
             System.out.println(number);
