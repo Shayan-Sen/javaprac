c0d9b5265a06c09eb20399dcc5d7d6cdb3ce94ce
Shayan Sen <senshayan2103@gmail.com>
2025-10-15 09:32:08 +0530

    add: implement main program in Fifteen.java

---
+++ accenture/C6_2/Fifteen.java
@@ -0,0 +1,15 @@
+package accenture.C6_2;
+
+public class Fifteen {
+
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5};
+        String[] words = {"Ignition sequence start!","Liftoff"};
+
+        System.out.println(words[0]);
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+        System.out.println(words[1]);
+    }
+}
