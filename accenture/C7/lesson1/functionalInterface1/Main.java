2fd32db441888ce66443a125f5902df6590b1c37
Shayan Sen <senshayan2103@gmail.com>
2025-10-19 03:14:21 +0530

    add: enhance Main class with lambda and subclass Runnable implementations for demonstration

--- accenture/C7/lesson1/functionalInterface1/Main.java
+++ accenture/C7/lesson1/functionalInterface1/Main.java
@@ -1,10 +1,22 @@
 package accenture.C7.lesson1.functionalInterface1;
 
 public class Main {
+
+    private final Runnable withLambda = () -> System.out.println("From Lambda:" + this.getClass());
+    private final Runnable withSubClass = new Runnable() {
+        @Override
+        public void run(){
+            System.out.println("From SubClass:" + this.getClass());
+        }
+    };
     public static void main(String[] args) {
+        Main main = new Main();
         BinaryOperator add = (a, b) -> a + b;
         BinaryOperator multiply = (a, b) -> a * b;
         System.out.printf("The addtion of %d and %d is %d.\nThe multiplication of %d and %d is %d.\n", 4, 3,
                 add.apply(4, 3), 5, 6, multiply.apply(5, 6));
+        System.out.println();
+        main.withLambda.run();
+        main.withSubClass.run();
     }
 }
