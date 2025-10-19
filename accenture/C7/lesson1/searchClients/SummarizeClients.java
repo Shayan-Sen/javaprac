68c2522d6d558ddfa81d880e3d60b92e9be69336
Shayan Sen <senshayan2103@gmail.com>
2025-10-19 20:38:27 +0530

    refactor: improve code formatting and readability in SummarizeClients class

--- accenture/C7/lesson1/searchClients/SummarizeClients.java
+++ accenture/C7/lesson1/searchClients/SummarizeClients.java
@@ -16,17 +16,18 @@ public final class SummarizeClients {
 
         int numClients = clients.size();
         int totalQuarterlySpend = clients.stream().mapToInt(SearchClients::getQuarterlyBudget).sum();
-        long nextExpiration = clients.stream().min(Comparator.comparing(SearchClients::getContractEnd)).map(SearchClients::getId).orElse(-1L);
+        long nextExpiration = clients.stream().min(Comparator.comparing(SearchClients::getContractEnd))
+                .map(SearchClients::getId).orElse(-1L);
         double averageBudget = clients.stream().mapToDouble(SearchClients::getQuarterlyBudget).average().orElse(0);
-        Set<ZoneId> representedZoneIds = clients.stream().flatMap(c -> c.getTimeZones().stream()).collect(Collectors.toSet());
-        Map<Year, Long> contractsPerYear = clients.stream().collect(Collectors.groupingBy(SummarizeClients::getContractYear,Collectors.counting()));
-
-        
+        Set<ZoneId> representedZoneIds = clients.stream().flatMap(c -> c.getTimeZones().stream())
+                .collect(Collectors.toSet());
+        Map<Year, Long> contractsPerYear = clients.stream()
+                .collect(Collectors.groupingBy(SummarizeClients::getContractYear, Collectors.counting()));
 
         System.out.println("Num clients: " + numClients);
         System.out.println("Total quarterly spend: " + totalQuarterlySpend);
         System.out.println("Average budget: " + averageBudget);
-        System.out.println("ID of next expiring contract: " + nextExpiration );
+        System.out.println("ID of next expiring contract: " + nextExpiration);
         System.out.println("Client time zones: " + representedZoneIds);
         System.out.println("Contracts per year: " + contractsPerYear);
     }
