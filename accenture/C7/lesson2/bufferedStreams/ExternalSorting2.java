cb1e4b32e27823b467217f4f5858db0bf558101e
Shayan Sen <senshayan2103@gmail.com>
2025-10-22 19:16:34 +0530

    refactor: improve code readability and streamline input/output handling in ExternalSorting2

--- accenture/C7/lesson2/bufferedStreams/ExternalSorting2.java
+++ accenture/C7/lesson2/bufferedStreams/ExternalSorting2.java
@@ -1,8 +1,8 @@
 package accenture.C7.lesson2.bufferedStreams;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,62 +10,65 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.PriorityQueue;
-import java.util.stream.Collectors;
 
 public class ExternalSorting2 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         if (args.length != 2) {
-            System.out.println("Usage: ExternalSorting2 [input folder] [output file]");
+            System.out.println("Usage: ExternalSorting2 [input dir] [output file]");
             return;
         }
-        List<Path> inputs = Files.walk(Path.of(args[0]), 1).skip(1).collect(Collectors.toList());
-        Path output = Path.of(args[1]);
-        List<BufferedReader> readerList = new ArrayList<BufferedReader>(inputs.size());
+
+        List<Path> inputs = Files.walk(Path.of(args[0]),1).skip(1).toList();
+        List<BufferedReader> readers = new ArrayList<BufferedReader>(inputs.size());
+        PriorityQueue<WordEntry> words = new PriorityQueue<WordEntry>();
+        Path outPath = Path.of(args[1]);
         try {
             for (Path input : inputs) {
-                readerList.add(Files.newBufferedReader(input, StandardCharsets.UTF_8));
+                readers.add(Files.newBufferedReader(input,StandardCharsets.UTF_8));
             }
-            PriorityQueue<WordEntry> queue = new PriorityQueue<WordEntry>();
-            for (BufferedReader reader : readerList) {
+            for(BufferedReader reader: readers){
                 String word = reader.readLine();
-                if (word != null) {
-                    queue.add(new WordEntry(word, reader));
-                }
+                words.add(new WordEntry(word, reader));
             }
-            try (Writer writer = Files.newBufferedWriter(output)) {
-                while (!queue.isEmpty()) {
-                    WordEntry entry = queue.poll();
+
+            try (BufferedWriter writer = Files.newBufferedWriter(outPath);) {
+                while (!words.isEmpty()) {
+                    WordEntry entry = words.poll();
                     writer.write(entry.word);
                     writer.write(System.lineSeparator());
                     String word = entry.reader.readLine();
-                    if (word != null) {
-                        queue.add(new WordEntry(word, entry.reader));
+                    if (word!=null) {
+                        words.add(new WordEntry(word,entry.reader));
                     }
                 }
             }
-        } finally {
-            for (BufferedReader reader : readerList) {
-                try {
-                    reader.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }finally{
+            if (!readers.isEmpty()) {
+                for (BufferedReader reader : readers) {
+                    try{ reader.close(); }
+                    catch(IOException e){ e.printStackTrace(); }
                 }
             }
         }
+
     }
 
-    private static final class WordEntry implements Comparable<WordEntry> {
+    private static final class WordEntry implements Comparable<WordEntry>{
         private final String word;
         private final BufferedReader reader;
-
-        private WordEntry(String word, BufferedReader reader) {
-            this.word = Objects.requireNonNull(word);
+        
+        private WordEntry(String word, BufferedReader reader){
             this.reader = Objects.requireNonNull(reader);
+            this.word = Objects.requireNonNull(word);
         }
 
         @Override
-        public int compareTo(WordEntry other) {
+        public int compareTo(WordEntry other){
             return word.compareTo(other.word);
         }
+        
     }
 }
